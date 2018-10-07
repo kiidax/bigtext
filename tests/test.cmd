03900d23f8cd11f419b10ad9c2830c187bd181bc
Katsuya Iida <kiidax@outlook.com>
2018-10-07 12:06:15 +0900

    multi column mode of vocab

--- tests/test.cmd
+++ tests/test.cmd
@@ -1,3 +1,22 @@
 del result.txt
+..\x64\Release\boar.exe vocab ..\boar\test2.txt -c 1 result.txt
+
+exit /b
+
 ..\x64\Release\boar.exe sample -r0.1 test.txt result.txt
+..\x64\Release\boar.exe sample -s -c 3 test.txt test2.txt -o result.txt
+..\x64\Release\boar.exe vocab -c ..\boar\test2.txt
+..\x64\Release\boar.exe vocab ..\boar\test2.txt
+..\x64\Release\boar.exe vocab -c ..\boar\test2.txt
+..\x64\Release\boar.exe vocab ..\boar\test2.txt
+
+del result.txt
+..\x64\Release\boar.exe sample -c ..\boar\test2.txt - 0.1 result.txt
+del result.txt
+..\x64\Release\boar.exe sample ..\boar\test2.txt - 0.1 result.txt
+del result.txt
+..\x64\Release\boar.exe sample -c ..\boar\test2.txt - 0.1 result.txt
+del result.txt
+..\x64\Release\boar.exe sample ..\boar\test2.txt - 0.1 result.txt
+
 python gentest.py verify
