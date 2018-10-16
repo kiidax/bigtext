e60ce36615e153d3766b9f34addfe0c10fd9a550
Katsuya Iida <kiidax@outlook.com>
2018-10-17 08:11:58 +0900

    Target number of lines in sampling mode

--- tests/test.cmd
+++ tests/test.cmd
@@ -1,8 +1,11 @@
 del result.txt
-..\x64\Release\boar.exe vocab ..\boar\test2.txt -c 1 result.txt
+
+..\x64\Release\boar.exe sample ..\boar\test2.txt test.txt -n 1000 result.txt
+..\x64\Release\boar count result.txt
 
 exit /b
 
+..\x64\Release\boar.exe vocab ..\boar\test2.txt -c 1 result.txt
 ..\x64\Release\boar.exe sample -r0.1 test.txt result.txt
 ..\x64\Release\boar.exe sample -s -c 3 test.txt test2.txt -o result.txt
 ..\x64\Release\boar.exe vocab -c ..\boar\test2.txt
